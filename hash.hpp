2d19f66fa3ed5041e0879c55bf33309564bfece6
Andrew Sutton <asutton@lock3software.com>
2021-02-28 14:08:22 -0500

    Make hash_append a CPO.

--- hash.hpp
+++ hash.hpp
@@ -65,159 +65,156 @@ namespace hashing
 
   // hash_append
 
-  /// Hash for trivially comparable T.
-  template<hash_algorithm H, std::integral T>
-  void hash_append(H& hash, T t)
-  {
-    hash(&t, sizeof(t));
-  }
-
-  template<hash_algorithm H, sa::enumeral T>
-  void hash_append(H& hash, T t)
-  {
-    hash(&t, sizeof(t));
-  }
-
-  /// Hash for floating point T. Guarantee that 0 and -0 have the same
-  /// hash code since 0 == -0.
-  template<hash_algorithm H, std::floating_point T>
-  void hash_append(H& hash, T t)
-  {
-    if (t == 0)
-      t = 0;
-    hash(&t, sizeof(t));
-  }
-
-  /// Hash for pointers. This just hashes the bits of the address, not the
-  /// indirect object. To hash the indirect object, provide an overload of
-  /// hash_append for that specific type.
-  template<hash_algorithm H, typename T>
-  void hash_append(H& hash, T* p)
-  {
-    hash(&p, sizeof(p));
-  }
-
-  /// Hash append for nullptr.
-  template<hash_algorithm H>
-  inline void hash_append(H& hash, std::nullptr_t p)
-  {
-    hash(&p, sizeof(p));
-  }
-
-  // Generic, structurally recursive hash append.
-  //
-  // NOTE: Like all recursive algorithms on composite tree structures, we either
-  // need to declare the generic version before its various implementations,
-  // or we declare all the various implementations before this function's
-  // definition. This uses the former.
-  template<hash_algorithm H, typename T>
-  void hash_append(H& hash, T const& obj);
-
   namespace detail
   {
     // Satisfied if `T` provides a hash-append member for `H`.
     template<typename T, typename H>
-    concept custom_member_hashable =
+    concept member_hashable =
       requires (T const& t, H& hash)
       {
         t.hash_append(hash);
       };
     
-    // Hash append for classes that customize hash append as a member.
-    template<hash_algorithm H, custom_member_hashable<H> T>
-    void hash_append_custom_member(H& hash, T const& obj)
-    {
-      obj.hash_append(hash);
-    }
-
-    // FIXME: This doesn't work because the lookup for hash_append can find
-    // the wrong thing, resulting in infinite recursion.
+    // Satisfied if `T` can be hashed.
     template<typename T, typename H>
-    concept custom_adl_hashable =
+    concept adl_hashable =
       requires (T const& t, H& hash)
       {
         hash_append(hash, t);
       };
 
-    // Hash append for classes that customize hash append as a non-member or
-    // friend definition.
-    template<hash_algorithm H, custom_adl_hashable<H> T>
-    void hash_append_custom_adl(H& hash, T const& obj)
+  } // namespace detail
+
+  struct hash_append_fn
+  {
+    /// Append the bits of integral types.
+    template<hash_algorithm H, std::integral T>
+    void operator()(H& hash, T t) const noexcept
+    {
+      hash(&t, sizeof(t));
+    }
+
+    /// Append the bits of enumeration types.
+    template<hash_algorithm H, sa::enumeral T>
+    void operator()(H& hash, T t) const noexcept
+    {
+      hash(&t, sizeof(t));
+    }
+
+    /// Append the bits of floating point types, but guarantee that 0 and -0
+    /// have the same hash code since 0 == -0.
+    template<hash_algorithm H, std::floating_point T>
+    void operator()(H& hash, T t) const noexcept
+    {
+      if (t == 0)
+        t = 0;
+      hash(&t, sizeof(t));
+    }
+
+    /// Append the bits of a pointer by hashing its representation, not the
+    /// bits of the object pointed at (i.e., no indirection is performed).
+    template<hash_algorithm H, typename T>
+    void operator()(H& hash, T* p) const noexcept
+    {
+      hash(&p, sizeof(p));
+    }
+
+    /// Hash the bits of the nullptr constant.
+    template<hash_algorithm H>
+    inline void operator()(H& hash, std::nullptr_t p) const noexcept
+    {
+      hash(&p, sizeof(p));
+    }
+
+    /// Hash the contents of almost everything else.
+    ///
+    /// NOTE: Concepts are great when alternatives operate on either disjoint
+    /// sets of types, or when refinement is clearly involved. When that isn't
+    /// the case---when the sets of types overlap---we can only choose behaviors
+    /// based on some preference. That's where constexpr if comes into play.
+    template<hash_algorithm H, typename T>
+    void operator()(H& hash, T const& obj) const noexcept
+    {
+      if constexpr (detail::member_hashable<T, H>)
+        append_using_member(hash, obj);
+      else if constexpr (std::ranges::range<T>)
+        append_range(hash, obj);
+      else if constexpr (sa::destructurable<T>)
+        append_destructurable(hash, obj);
+      else if constexpr (sa::basic_data_type<T>)
+        append_data_type(hash, obj);
+      else if constexpr (detail::adl_hashable<T, H>)
+        append_using_adl(hash, obj);
+      else
+        static_assert(sa::dependent_false<T>(), "no matching overload");
+    }
+
+    // Append for member customization.
+    template<hash_algorithm H, detail::member_hashable<H> T>
+    void append_using_member(H& hash, T const& obj) const noexcept
+    {
+      obj.hash_append(hash);
+    }
+
+    // Append using ADL lookup to find non-member definitions or friend function
+    // definitions.
+    //
+    // NOTE: This is somewhat fragile. If this wasn't a customization point
+    // object, the "most generic" version of `hash_append` would need to be
+    // declared before the "dispatcher". In that case, calls to `hash_append`
+    // could bind to that name for non-matching cases. The result is infinite
+    // recursion.
+    //
+    // The CPO approach works (presumably) because the hash_append has not
+    // yet been declared in this scope.
+    template<hash_algorithm H, detail::adl_hashable<H> T>
+    void append_using_adl(H& hash, T const& obj) const noexcept
     {
       hash_append(hash, obj);
     }
 
     // Hash append for ranges.
     template<hash_algorithm H, std::ranges::range T>
-    void hash_append_range(H& hash, T const& range)
+    void append_range(H& hash, T const& range) const noexcept
     {
       std::size_t count = 0;
       for (auto const& elem : range) {
-        hash_append(hash, elem);
+        operator()(hash, elem);
         ++count;
       }
-      hash_append(hash, count);
+      operator()(hash, count);
     }
 
     // Hash append for arrays, tuples, and other simple classes.
     template<hash_algorithm H, sa::destructurable T>
-    void hash_append_destructurable(H& hash, T const& obj)
+    void append_destructurable(H& hash, T const& obj) const noexcept
     {
       std::size_t count = 0;
       template for (auto const& member : obj) {
-        hash_append(hash, member);
+        operator()(hash, member);
         ++count;
       }
-      hash_append(hash, count);
+      operator()(hash, count);
     }
 
     // Hash append for basic data types in an application domain.
     template<hash_algorithm H, sa::basic_data_type T>
-    void hash_append_data_type(H& hash, T const& obj)
+    void append_data_type(H& hash, T const& obj) const noexcept
     {
       namespace meta = std::experimental::meta;
       std::size_t count = 0;
       constexpr auto subobjects = meta::subobjects_of(^T, meta::is_data_member);
       template for (constexpr meta::info sub : subobjects) {
-        hash_append(hash, obj.[:sub:]);
+        operator()(hash, obj.[:sub:]);
         ++count;
       }
-      hash_append(hash, count);
+      operator()(hash, count);
     }
-  } // namespace detail
+  };
 
-  // Hash classes or destructurable types.
-  //
-  // These constraints define overlapping sets of types. Some destructurable
-  // types are also classes.
-  //
-  // NOTE: Concepts are great when alternatives operate on either disjoint sets
-  // of types, or when refinement is clearly involved. When that isn't the
-  // case---when the sets of types overlap---we can only choose behaviors based
-  // on some preference. That's where constexpr if comes into play.
-  //
-  // NOTE: The ADL case is *bad* if you put it in the wrong place. The ADL
-  // lookup can find the main `hash_append` function, causing lookup to succeed
-  // but generate infinite recursion. Here, we make that the fallback case,
-  // which forces a late error.
-  template<hash_algorithm H, typename T>
-  void hash_append(H& hash, T const& obj)
-  {
-    if constexpr (detail::custom_member_hashable<T, H>)
-      detail::hash_append_custom_member(hash, obj);
-    else if constexpr (std::ranges::range<T>)
-      detail::hash_append_range(hash, obj);
-    else if constexpr (sa::destructurable<T>)
-      detail::hash_append_destructurable(hash, obj);
-    else if constexpr(sa::basic_data_type<T>)
-      detail::hash_append_data_type(hash, obj);
-    // else if constexpr (detail::custom_adl_hashable<T, H>)
-    //   detail::hash_append_custom_adl(hash, obj);
-    else
-      static_assert(sa::dependent_false<T>(), "no matching overload");
-  }
+  constexpr hash_append_fn hash_append;
 
-  // hash
+  // hash object
 
   /// Computes the hash values of objects.
   template<typename T>
